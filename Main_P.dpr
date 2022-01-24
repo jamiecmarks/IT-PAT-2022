8cad4954063f3eb4027ecb562445e1591bd02003
JAMIE cartwright marks <marksjamie123@gmail.com>
2022-01-24 16:11:04 +0200

    First commit

---
+++ Main_P.dpr
@@ -0,0 +1,14 @@
+program Main_P;
+
+uses
+  Forms,
+  Splash_U in 'Splash_U.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
